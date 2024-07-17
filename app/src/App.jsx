15fd1e857beb3d85b5fe87a5e767d898c0767576
milan-sharma-21 <milansharma.id@gmail.com>
2024-07-17 12:24:47 +0530

    Commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
-const API_KEY = "sk-None-e5gvvb3uTY5IL9Fd4sEnT3BlbkFJtQr6Zjvv2QlDGrCoCvDB"
+const API_KEY = "API_KEY_HERE"
 const systemMessage = { 
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
 }
